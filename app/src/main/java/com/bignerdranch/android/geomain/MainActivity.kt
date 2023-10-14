c50eeeb03714d939c4f4e1ff3e69b3357be94c16
EkatTk <ektk1805@gmail.com>
2023-10-14 10:52:29 +0700

    Доп.задания

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.geomain
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -25,21 +28,24 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia,true))
     private var currentIndex = 0
-
+    private var correctChek = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
+        if (savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt("saveIndex", 0)
+            correctChek = savedInstanceState.getInt("saveAnswer", 0)
+        }
         setContentView(R.layout.activity_main)
-        trueButton =
-            findViewById(R.id.true_button)
-        falseButton =
-            findViewById(R.id.false_button)
+        trueButton = findViewById(R.id.true_button)
 
-        nextButton =
-            findViewById(R.id.next_button)
-        questionTextView =
-            findViewById(R.id.question_text_view)
+        falseButton = findViewById(R.id.false_button)
+
+        nextButton = findViewById(R.id.next_button)
+        nextButton.visibility = View.INVISIBLE
+
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
@@ -49,53 +55,81 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) %
-                    questionBank.size
+            currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
         updateQuestion()
     }
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,
-            "onStart() called")
+        Log.d(TAG, "onStart() called")
     }
     override fun onResume() {
         super.onResume()
-        Log.d(TAG,
-            "onResume() called")
+        Log.d(TAG, "onResume() called")
     }
     override fun onPause() {
         super.onPause()
-        Log.d(TAG,
-            "onPause() called")
+        Log.d(TAG, "onPause() called")
     }
     override fun onStop() {
         super.onStop()
-        Log.d(TAG,
-            "onStop() called")
+        Log.d(TAG,"onStop() called")
     }
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,
-            "onDestroy() called")
+        Log.d(TAG,"onDestroy() called")
     }
-
 
     private fun updateQuestion() {
-        val questionTextResId =
-            questionBank[currentIndex].textResId
+        val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        nextButton.visibility = View.INVISIBLE
+        trueButton.visibility=View.VISIBLE
+        falseButton.visibility=View.VISIBLE
     }
     private fun checkAnswer(userAnswer: Boolean) {
+        nextButton.visibility = View.VISIBLE
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
+            correctChek++
         } else {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+        trueButton.visibility=View.INVISIBLE
+        falseButton.visibility=View.INVISIBLE
+
+        if (currentIndex == questionBank.size-1){
+            nextButton.visibility = View.INVISIBLE
+            showCustomDialog(correctChek.toString())
+        }
+2
+    }
+
+    private fun showCustomDialog(data: String) {
+        val dialogBinding = layoutInflater.inflate(R.layout.activity_modal, null)
+        val myDialog = Dialog(this)
+        myDialog.setContentView(dialogBinding)
+        myDialog.setCancelable(true)
+        myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        myDialog.show()
+        val resultText = myDialog.findViewById<TextView>(R.id.result_text)
+        val str = "Вы ответили правильно на " + data + " вопросов из " + questionBank.size
+        resultText.text = str
+        val okButton = dialogBinding.findViewById<Button>(R.id.ok_button)
+        okButton.setOnClickListener {
+            myDialog.dismiss()
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.d(TAG,"onSaveInstanceState() called")
+        savedInstanceState.putInt("saveIndex", currentIndex)
+        savedInstanceState.putInt("saveAnswer", correctChek)
     }
 
 }
