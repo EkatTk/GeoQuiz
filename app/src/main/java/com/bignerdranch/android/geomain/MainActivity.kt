0965e8fbb2ce1b97e9bd71598b901fcbd2d965a9
EkatTk <ektk1805@gmail.com>
2024-02-11 18:53:12 +0700

    От Button к ImageButton

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,8 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var backButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var backButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -43,11 +44,13 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
+        nextButton = findViewById(R.id.next_button)
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
 
+        backButton = findViewById(R.id.back_button)
         backButton.setOnClickListener {
             currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
