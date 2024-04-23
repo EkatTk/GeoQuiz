b462bc5354c37d808e50158a8cdc7833681d9b14
EkatTk <ektk1805@gmail.com>
2024-04-23 17:35:40 +0700

    Упражнение 1. Изменение расположения уведомления

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geomain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -17,21 +18,23 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
+            val toast = Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT)
-                .show()
+            toast.setGravity(Gravity.TOP, 0, 100)
+            toast.show()
         }
 
         falseButton = findViewById(R.id.false_button)
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
+            val toast =  Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT)
-                .show()
+            toast.setGravity(Gravity.TOP, 0, 100)
+            toast.show()
         }
 
     }
