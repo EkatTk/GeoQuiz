6596733c9ed6a6f75ca57d328097deaa63944072
EkatTk <ektk1805@gmail.com>
2024-04-23 21:18:49 +0700

    добавление отображении версии api

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.lifecycle.ViewModelProviders
 private const val TAG = "MainActivity"
 private const val KEY_INDEX1 = "currentIndex"
 private const val KEY_INDEX2 = "correctChek"
+private const val KEY_INDEX3 = "isCheat"
 private const val REQUEST_CODE_CHEAT = 0
 class MainActivity : AppCompatActivity() {
 
@@ -106,6 +107,7 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "onSaveInstanceState")
         savedInstanceState.putInt(KEY_INDEX1, quizViewModel.currentIndex)
         savedInstanceState.putInt(KEY_INDEX2, quizViewModel.correctChek)
+        savedInstanceState.putBoolean(KEY_INDEX3, quizViewModel.isCheater)
     }
     override fun onStop() {
         super.onStop()
@@ -121,7 +123,7 @@ class MainActivity : AppCompatActivity() {
         nextButton.isEnabled = false
         trueButton.isEnabled = true
         falseButton.isEnabled = true
-        cheatButton.visibility = View.VISIBLE
+        cheatButton.isEnabled = true
         quizViewModel.isCheater = false
     }
 
